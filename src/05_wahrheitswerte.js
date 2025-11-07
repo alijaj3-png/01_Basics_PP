3b2c179eba0a38bbe5abe2ccf6bf4d9179d555be
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-11-07 10:16:43 +0000

    05 / Wahrheitswerte basic

---
+++ src/05_wahrheitswerte.js
@@ -0,0 +1,17 @@
+
+/* Berechnung Alter */
+
+// Deklaration
+let ageJohn, ageMark;
+const birthYearJohn = 2000;
+const birthYearMark = 1990;
+
+// Berechnung Alter
+let year = 2025;
+ageJohn = year - birthYearJohn;
+ageMark = year - birthYearMark;
+
+// Ausgabe
+console.log("ageJohn: " + ageJohn);
+console.log("ageMark: " + ageMark);
+
