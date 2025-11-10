58cf0851a918176e3825359a979941fcac020928
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-11-10 11:54:02 +0000

    07 /  Lösungsbeispiel

--- src/07_codechallenge_II.js
+++ src/07_codechallenge_II.js
@@ -14,6 +14,31 @@ Alter 13-17 // Cola
 
 -->zB: "Max trinkt Cola."
 
+Bitte ggf. ein Struktogramm erstellen --> Structog
+
 /* Getränke-Challenge */
 
+const prompt = require('prompt-sync')({sigint: true});
 
+const firstName = prompt("Bitte Namen eingeben: ");
+const age =  parseInt(prompt("Bitte Alter eingeben: ")); // "2" --> 2
+const cond = true;
+
+
+switch (cond) {
+    case (age >= 0) && (age <= 5) :
+        console.log(firstName + " trinkt Milch.");
+        break;
+    case (age >= 6) && (age <= 12):
+        console.log(firstName + " trinkt Saft.");
+        break;
+    case (age >= 13) && (age <= 17):
+        console.log(firstName + " trinkt Cola.");
+        break;
+    case (age >= 18) && (age <= 130):
+        console.log(firstName + " trinkt Wein.");
+        break;   
+    default:
+        console.log("Bitte Tee trinken!");   
+        break;
+}
