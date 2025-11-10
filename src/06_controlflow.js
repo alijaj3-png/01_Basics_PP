ac563db5b20f14b348363892e3659cb6c2b0bb50
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-11-10 08:18:14 +0000

    06 / IF

--- src/06_controlflow.js
+++ src/06_controlflow.js
@@ -1,7 +1,7 @@
 // Entscheidungsstrukturen | control flow
 
 // Deklaration + Assignment
-const ageJohn = 25;
+const ageJohn = 31;
 const ageMark = 30;
 
 // Deklaration
@@ -14,7 +14,20 @@ isJohnEqual = (ageJohn == ageMark);
 // Ausgabe
 console.log("ageJohn: " + ageJohn);
 console.log("ageMark: " + ageMark);
-console.log("isJohnOlder: " + isJohnOlder);
-console.log("isJohnEqual: " + isJohnEqual);
-console.log("------------------");
+// console.log("isJohnOlder: " + isJohnOlder);
+// console.log("isJohnEqual: " + isJohnEqual);
+// console.log("------------------");
+
+/************ IF  ************/
+// TINA --> There is no alternative!
+// entweder JA oder nix ... alternativlos
+// Türsteher-Politik (Doors 10 p.m.)
+
+// if(true)             // Wahrheitswert(e)
+// if(false)
+// if(isJohnOlder)      // Verweis
+if(ageJohn > ageMark)   // direkter Test
+{
+    console.log("John ist älter.");
+}
 
