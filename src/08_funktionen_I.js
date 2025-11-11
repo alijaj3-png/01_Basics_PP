9c0a1a5adc3d61de04fc2e33842c99e6f7595019
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-11-11 09:19:27 +0000

    08 / 2b. Parametrisierung + Datenübergabe von AUSSEN

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -27,3 +27,14 @@ function ausgabeNamen() {
 }
 
 // console.log(firstName); // Fehler : scope!
+
+/***** Funktionen 02b *****/
+// 2b. Parametrisierung + Datenübergabe von AUSSEN
+
+ausgabeNamen2("Kevin"); // Argument --> Daten für Parameter
+ausgabeNamen2("Klaus");
+ausgabeNamen2("Ibrahim");
+
+function ausgabeNamen2(firstName) {  // Parameter
+    console.log("Hallo, " + firstName + "!");
+}
