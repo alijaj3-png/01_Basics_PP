363b91dda06c0e3f408c788f86c4537d53ed2840
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-11-12 11:39:16 +0000

    10 / Dateneingabe startApp()

--- src/10_funktionen_II.js
+++ src/10_funktionen_II.js
@@ -13,17 +13,41 @@
 const ERROR_STR_DIV = "Teilen durch 0 nicht möglich";
 const ERROR_STR_GEN = "Irgendwas ging schief!";
 
+/** Dateneingabe */
+
+const prompt = require('prompt-sync')({sigint: true});
+
+startApp();
+
+function startApp() {
+	output(calculator(getNum1(),getNum2(),getOp()));
+}
+
+function getNum1() {
+	return parseInt(prompt("Zahl1?: "));
+}
+
+function getNum2() {
+	return parseInt(prompt("Zahl2?: "));
+}
+
+function getOp() {
+	return prompt("OP?: ");
+}
+
+
+
 /** WAS soll gemacht werden? */
 
 // module: calculator | tests:
 // agreement : "+","-","*",":","/"
-output(calculator(3,2,"+"));
-output(calculator(3,2,"-"));
-output(calculator(3,2,"*"));
-output(calculator(3,2,":"));
-output(calculator(3,2,"/"));
-output(calculator(3,0,"/"));
-output(calculator(3,2,"#?!"));
+// output(calculator(3,2,"+"));
+// output(calculator(3,2,"-"));
+// output(calculator(3,2,"*"));
+// output(calculator(3,2,":"));
+// output(calculator(3,2,"/"));
+// output(calculator(3,0,"/"));
+// output(calculator(3,2,"#?!"));
 function calculator(a,b,op) {
 	switch (op) {
 		case "+":
