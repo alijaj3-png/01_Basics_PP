94573f252105c8ba3879e21dfb2850eae1a22eb2
Peter Schmidt <schmidt.multimedia@gmail.com>
2025-11-12 11:27:50 +0000

    10 / calculator();

--- src/10_funktionen_II.js
+++ src/10_funktionen_II.js
@@ -5,13 +5,42 @@
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 
 1. Dateneingabe + -überprüfung :  
-2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+2. Auswahl Rechenart : check!
+3. Fkt. Grundrechenarten : check!
 4. Ausgabe in Konsole : check!
 */
 
 const ERROR_STR_DIV = "Teilen durch 0 nicht möglich";
+const ERROR_STR_GEN = "Irgendwas ging schief!";
 
+/** WAS soll gemacht werden? */
+
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+output(calculator(3,2,"+"));
+output(calculator(3,2,"-"));
+output(calculator(3,2,"*"));
+output(calculator(3,2,":"));
+output(calculator(3,2,"/"));
+output(calculator(3,0,"/"));
+output(calculator(3,2,"#?!"));
+function calculator(a,b,op) {
+	switch (op) {
+		case "+":
+			return add(a,b);
+		case "-":
+			return subtract(a,b);
+		case "*":
+			return multiply(a,b);
+		case ":":
+		case "/":
+			return divide(a,b);
+		default:
+			return ERROR_STR_GEN;
+	}
+}
+
+/** WIE sollen die Aufgeben gelöst werden? */
 
 // module: division a / b |  test:
 // output(divide(4,2));
